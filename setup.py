8294369dfeef8fe39f9d8e63a9f9df021a3e1578
maerd47 <78052898+maerd47@users.noreply.github.com>
2026-03-31 13:36:57 -0400

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Bank demo',
+    version= '0.0.0',
+    author= 'sam',
+    author_email= 'buzzworm47@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
